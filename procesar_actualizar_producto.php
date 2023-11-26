6ca5013e78a9d64db9f11edd23340222e7046ad4
Javier Ramirez <javierbaquero513@gmail.com>
2023-11-26 17:05:42 -0500

    actualizaion del codigo entreda

--- procesar_actualizar_producto.php
+++ procesar_actualizar_producto.php
@@ -1,71 +1,88 @@
 <?php
-session_start();
-
-if (!isset($_SESSION['usuario'])) {
-    header("location: index.php");
-    exit();
-}
-
 include 'conexion_be.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recuperar datos del formulario
-    $codigo = $_POST['codigo_salida'];
-    $cantidad_salida = $_POST['cantidad_salida'];
+    // Recuperar datos del formulario y validarlos
+    $codigo_actualizar = mysqli_real_escape_string($conexion, $_POST['codigo_actualizar']);
+    $nueva_cantidad = intval($_POST['nueva_cantidad']);
 
-    // Obtener el total existente
-    $query_existente = "SELECT total FROM productos WHERE codigo = ?";
+    // Verificar si el código existe en la base de datos
+    $verificar_codigo = mysqli_query($conexion, "SELECT * FROM productos WHERE codigo = '$codigo_actualizar'");
+
+    if (mysqli_num_rows($verificar_codigo) == 0) {
+        // Código no existe, mostrar mensaje y salir
+        echo '<script>alert("Código de producto no encontrado. Por favor, ingrese uno válido."); window.location = "bienvenido.php";</script>';
+        exit();
+    }
+
+    // Obtener la cantidad actual y el total existente
+    $query_existente = "SELECT cantidad_entrada, total FROM productos WHERE codigo = ?";
     $stmt_existente = mysqli_prepare($conexion, $query_existente);
-    mysqli_stmt_bind_param($stmt_existente, "s", $codigo);
+    mysqli_stmt_bind_param($stmt_existente, "s", $codigo_actualizar);
     mysqli_stmt_execute($stmt_existente);
-    $result_existente = mysqli_stmt_get_result($stmt_existente);
+    mysqli_stmt_store_result($stmt_existente);
 
-    if ($result_existente) {
-        $row_existente = mysqli_fetch_assoc($result_existente);
+    if (mysqli_stmt_num_rows($stmt_existente) > 0) {
+        mysqli_stmt_bind_result($stmt_existente, $cantidad_entrada_existente, $total_existente);
+        mysqli_stmt_fetch($stmt_existente);
 
-        if ($row_existente) {
-            $total_existente = $row_existente['total'];
+        // Calcular el nuevo total y la nueva cantidad
+        $nueva_cantidad_total = $cantidad_entrada_existente + $nueva_cantidad;
+        $nuevo_total = $total_existente + $nueva_cantidad;
 
-            // Verificar si hay suficiente cantidad para realizar la salida
-            if ($total_existente >= $cantidad_salida) {
-                // Calcular el nuevo total
-                $total_nuevo = $total_existente - $cantidad_salida;
+        // Actualizar la cantidad y el total en la base de datos
+        $query_actualizar = "UPDATE productos SET cantidad_entrada = ?, total = ? WHERE codigo = ?";
+        $stmt_actualizar = mysqli_prepare($conexion, $query_actualizar);
+        mysqli_stmt_bind_param($stmt_actualizar, "iis", $nueva_cantidad_total, $nuevo_total, $codigo_actualizar);
+        $resultado_actualizar = mysqli_stmt_execute($stmt_actualizar);
 
-                // Actualizar el total en la tabla productos
-                $query_actualizar = "UPDATE productos SET total = ? WHERE codigo = ?";
-                $stmt_actualizar = mysqli_prepare($conexion, $query_actualizar);
-                mysqli_stmt_bind_param($stmt_actualizar, "is", $total_nuevo, $codigo);
-                $resultado_actualizar = mysqli_stmt_execute($stmt_actualizar);
+        if ($resultado_actualizar) {
+            // Verificar si ya existe un registro con el mismo código en entrada_nueva_producto
+            $query_verificar_existencia = "SELECT * FROM entrada_nueva_producto WHERE codigoProducto = ?";
+            $stmt_verificar_existencia = mysqli_prepare($conexion, $query_verificar_existencia);
+            mysqli_stmt_bind_param($stmt_verificar_existencia, "s", $codigo_actualizar);
+            mysqli_stmt_execute($stmt_verificar_existencia);
+            mysqli_stmt_store_result($stmt_verificar_existencia);
 
-                if ($resultado_actualizar) {
-                    // Insertar un nuevo registro en la tabla salida_cantidad_producto
-                    $query_insertar_salida = "INSERT INTO salida_cantidad_producto (codigoProducto, salidaProducto) VALUES (?, ?)";
-                    $stmt_insertar_salida = mysqli_prepare($conexion, $query_insertar_salida);
+            if (mysqli_stmt_num_rows($stmt_verificar_existencia) > 0) {
+                // Si ya existe, actualizar la cantidad
+                $query_actualizar_entrada = "UPDATE entrada_nueva_producto SET nuevaCantidadEntrada = nuevaCantidadEntrada + ? WHERE codigoProducto = ?";
+                $stmt_actualizar_entrada = mysqli_prepare($conexion, $query_actualizar_entrada);
+                mysqli_stmt_bind_param($stmt_actualizar_entrada, "is", $nueva_cantidad, $codigo_actualizar);
+                $resultado_actualizar_entrada = mysqli_stmt_execute($stmt_actualizar_entrada);
 
-                    if (!$stmt_insertar_salida) {
-                        die("Error al preparar la consulta de inserción de salida: " . mysqli_error($conexion));
-                    }
-
-                    mysqli_stmt_bind_param($stmt_insertar_salida, "ii", $codigo, $cantidad_salida);
-                    $resultado_insertar_salida = mysqli_stmt_execute($stmt_insertar_salida);
-
-                    if ($resultado_insertar_salida) {
-                        // Mostrar mensaje de éxito con un script JavaScript
-                        echo '<script>alert("Salida Registrada en la Base de Datos."); window.location = "bienvenido.php";</script>';
-                    } else {
-                        echo "Error al insertar en la tabla salida_cantidad_producto: " . mysqli_error($conexion);
-                    }
-                } else {
-                    echo "Error al actualizar la cantidad de salida: " . mysqli_error($conexion);
+                if (!$resultado_actualizar_entrada) {
+                    echo "Error al actualizar la entrada existente: " . mysqli_error($conexion);
                 }
             } else {
-                echo "No hay suficiente cantidad disponible para realizar la salida.";
+                // Si no existe, insertar un nuevo registro
+                $query_insertar = "INSERT INTO entrada_nueva_producto (codigoProducto, nuevaCantidadEntrada) VALUES (?, ?)";
+                $stmt_insertar = mysqli_prepare($conexion, $query_insertar);
+
+                if (!$stmt_insertar) {
+                    die("Error al preparar la consulta de inserción: " . mysqli_error($conexion));
+                }
+
+                mysqli_stmt_bind_param($stmt_insertar, "si", $codigo_actualizar, $nueva_cantidad);
+                $resultado_insertar = mysqli_stmt_execute($stmt_insertar);
+
+                if (!$resultado_insertar) {
+                    echo "Error al insertar en la tabla entrada_nueva_producto: " . mysqli_error($conexion);
+                }
             }
+
+            // Cerrar las consultas
+            mysqli_stmt_close($stmt_existente);
+            mysqli_close($conexion);
+
+            // Mostrar mensaje de éxito con un script JavaScript
+            echo '<script>alert("Dato actualizado exitosamente."); window.location = "bienvenido.php";</script>';
+            exit();
         } else {
-            echo "No se encontró ningún producto con el código '$codigo'.";
+            echo "Error al actualizar la cantidad: " . mysqli_error($conexion);
         }
     } else {
-        echo "Error al obtener el total existente: " . mysqli_error($conexion);
+        echo "No se encontró ningún producto con el código '$codigo_actualizar'.";
     }
 
     // Cerrar las consultas
